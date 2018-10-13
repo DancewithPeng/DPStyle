e1f94ec1b15b4d47f943b3fb305fafe7148f3e8e
Zhang Peng <ex-zhangpeng012@pingan.com.cn>
2018-10-13 17:38:23 +0800

    修改API风格，使其更简洁、统一

--- DPStyle/DPStyle/Source/DPFont.swift
+++ DPStyle/DPStyle/Source/DPFont.swift
@@ -6,64 +6,55 @@
 //  Copyright © 2018年 dancewithpeng@gmail.com. All rights reserved.
 //
 
-import UIKit
+import UIKit.UIFont
 
 
-/// 字重，为了封装size方法
-public class DPWeight {
-    
-    /// 对应的UIFont的字重
-    private let weight: UIFont.Weight
-    
-    /// 初始化方法，根据UIFont字重来初始化
-    ///
-    /// - Parameter weight: UIFont的字重
-    init(weight: UIFont.Weight) {
-        self.weight = weight
-    }
-    
-    /// 返回对应字号的系统字体
-    ///
-    /// - Parameter size: 字号
-    /// - Returns: 返回对应字号的系统字体
-    public func size(_ size: CGFloat) -> UIFont {        
-        return UIFont.systemFont(ofSize: size, weight: weight)
-    }
-}
-
-
-// MARK: - 对应的字重
+// MARK: - 返回各种字重的UIFont便捷方法
 public extension UIFont {
     
     /// 标准体
-    public static let regular       = DPWeight(weight: .regular)
+    public static func regular(size: CGFloat) -> UIFont {
+        return UIFont.systemFont(ofSize: size, weight: .regular)
+    }
     
     /// 超轻体
-    public static let ultraLight    = DPWeight(weight: .ultraLight)
+    public static func ultraLight(size: CGFloat) -> UIFont {
+        return UIFont.systemFont(ofSize: size, weight: .ultraLight)
+    }
     
     /// 细体
-    public static let thin          = DPWeight(weight: .thin)
+    public static func thin(size: CGFloat) -> UIFont {
+        return UIFont.systemFont(ofSize: size, weight: .thin)
+    }
     
     /// 轻体
-    public static let light         = DPWeight(weight: .light)
+    public static func light(size: CGFloat) -> UIFont {
+        return UIFont.systemFont(ofSize: size, weight: .light)
+    }
     
     /// 中体
-    public static let medium        = DPWeight(weight: .medium)
+    public static func medium(size: CGFloat) -> UIFont {
+        return UIFont.systemFont(ofSize: size, weight: .medium)
+    }
     
     /// 半粗体
-    public static let semibold      = DPWeight(weight: .semibold)
+    public static func semibold(size: CGFloat) -> UIFont {
+        return UIFont.systemFont(ofSize: size, weight: .semibold)
+    }
     
     /// 粗体
-    public static let bold          = DPWeight(weight: .bold)
+    public static func bold(size: CGFloat) -> UIFont {
+        return UIFont.systemFont(ofSize: size, weight: .bold)
+    }
     
     /// 重体
-    public static let heavy         = DPWeight(weight: .heavy)
+    public static func heavy(size: CGFloat) -> UIFont {
+        return UIFont.systemFont(ofSize: size, weight: .heavy)
+    }
     
     /// 黑体
-    public static let black         = DPWeight(weight: .black)
-    
-    public static func medium(size: CGFloat) -> UIFont {
-        return medium.size(size)
+    public static func black(size: CGFloat) -> UIFont {
+        return UIFont.systemFont(ofSize: size, weight: .black)
     }
 }
 
@@ -76,7 +67,7 @@ public extension UIFont {
     /// - Parameter size: 对应的字号
     /// - Returns: 返回对应字号的系统字体，字重为标准体
     public static func size(_ size: CGFloat) -> UIFont {
-        return regular.size(size)
+        return regular(size: size)
     }
 }
 
